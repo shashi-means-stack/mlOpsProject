1bb9d4a37ffe416d26b3b9668cd2fabcaea4cebc
shashi-means-stack <kumar.shashikant@gmail.com>
2025-09-11 01:54:30 +0530

    update readme file

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 from typing import List
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
+# with open('README.md', 'r', encoding='utf-8') as f:
+#     long_description = f.read()     
    
 
 __version__ = "0.0.1"
@@ -17,8 +17,8 @@ setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
-    long_description=long_description,
-    long_description_content="text/markdown",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
