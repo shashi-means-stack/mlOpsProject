75c427af33a2240a262e1ec2ea0ab54c2d6c3772
shashi-means-stack <kumar.shashikant@gmail.com>
2025-09-14 02:26:37 +0530

    update package name

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.3"
+__version__ = "0.1.0"
 REPO_NAME = "mlOpsProject"
-PKG_NAME= "TestPackage"
+PKG_NAME= "testpackage-shashi"
 AUTHOR_USER_NAME = "shashi-means-stack"
 AUTHOR_EMAIL = "kumar.shashikant@gmail.com"
 
